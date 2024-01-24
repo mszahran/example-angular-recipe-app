d6dc50eaf7edb40a6bd420ad97ba384c975d26c1
Zahran <muhammadzahran32@gmail.com>
2024-01-24 16:31:59 +0700

    membuat auto login agar ketika selesai login dan melakukan refresh halaman sesi tidak terlogout, dan membuat auto logout yang berguna untuk auto logout dalam waktu expires tertentu dan menghapus sessi untuk tombol logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
   user = new BehaviorSubject<UserModel | any>(null);
+  private tokeExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {
   }
@@ -50,17 +51,58 @@ export class AuthService {
     );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData') || '{}');
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new UserModel(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+
+      this.autoLogout(expirationDuration)
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+
+    localStorage.removeItem('userData');
+
+    if (this.tokeExpirationTimer) {
+      clearTimeout(this.tokeExpirationTimer);
+    }
+
+    this.tokeExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    console.log(expirationDuration);
+
+    this.tokeExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-
     const user = new UserModel(email, userId, token, expirationDate);
 
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
