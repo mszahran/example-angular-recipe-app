5a3698a18018c18a5c9a69ce2dd39a416812dc68
Zahran <muhammadzahran32@gmail.com>
2024-01-24 11:36:54 +0700

    membuat proses authentication apabila berhasil maka meredirect ke halaman recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,31 @@
 import {
-  Component
+  Component, OnDestroy, OnInit
 } from '@angular/core';
 import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs-compat";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
+  isAuthenticated = false;
 
-  constructor(private dataStorageService: DataStorageService) {
+  private userSub!: Subscription;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user; // its same like !user ? false : true
+
+      console.log(!user);
+      console.log(!!user);
+    });
   }
 
   onSaveData() {
@@ -20,5 +34,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
