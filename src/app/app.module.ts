ed6759309d021d8f172b0c990b24474e18dfb3ef
Zahran <muhammadzahran32@gmail.com>
2024-01-26 10:01:08 +0700

    menerapkan Core Module untuk semua service yang ada agar menjadikan kode pada app.module.ts menjadi lebih rapih

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AuthComponent} from "./auth/auth.component";
-import {RecipeService} from "./recipe/recipe.service";
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {RecipesModule} from "./recipe/recipes.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
 
 @NgModule({
   declarations: [
@@ -28,16 +26,8 @@ import {SharedModule} from "./shared/shared.module";
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
